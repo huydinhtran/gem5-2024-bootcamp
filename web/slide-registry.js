5100531c5a840620bf9e88515916ae1ad4cf037a
Noah Krim <nkrim62@gmail.com>
2024-07-27 05:10:04 -0700

    Website functionality, not arranged properly for gh-pages

---
+++ web/slide-registry.js
@@ -0,0 +1,16 @@
+window.get_slide_registry = function() {
+    return [
+        {
+            "name": "01-Introduction",
+            "slides": [
+                "01-simulation-background",
+                "02-getting-started",
+                "03-python-background"
+            ]
+        },
+        {
+            "name": "02-Using-gem5",
+            "slides": []
+        }
+    ];
+}
